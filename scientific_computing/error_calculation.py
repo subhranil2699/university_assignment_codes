7787c20db3190be89d2f085f9dcbf6c84aa276b2
Subhranil Sarkar <84725647+subhranil2699@users.noreply.github.com>
2022-05-26 21:09:26 +0530

    Update error_calculation.py

--- scientific_computing/error_calculation.py
+++ scientific_computing/error_calculation.py
@@ -1,16 +1,18 @@
 from typing import Dict
 
-
 str_float_dict = Dict[str, float]
 
-def errors(true_value: float, approx_value: float, round_: int) -> str_float_dict:
+
+def errors(true_value: float,
+           approx_value: float,
+           round_digit: int) -> str_float_dict:
     '''
     Calculate abs error, rel error and percn error upto 4 significant digits 
     '''
     
-    absolute_error: float = round(abs(true_value - approx_value), round_)
-    relative_error: float = round(absolute_error / true_value, round_)
-    percentage_error: str = round(relative_error * 100, round_)
+    absolute_error: float = round(abs(true_value - approx_value), round_digit)
+    relative_error: float = round(absolute_error / true_value, round_digit)
+    percentage_error: float = round(relative_error * 100, round_digit)
     
     result: str_float_dict =  {
         "absolute_error": absolute_error,
@@ -21,11 +23,13 @@ def errors(true_value: float, approx_value: float, round_: int) -> str_float_dic
     return result
 
 
-true_value: float = float(input("True value: "))
-approx_value: float = float(input("Appx. value: "))
-round_ : int = int(input("Round: "))
 
-calc_errors: str_float_dict = errors(true_value, approx_value, round_)
+if __name__ == "__main__":
+    true_value: float = float(input("True value: "))
+    approx_value: float = float(input("Apprx value: "))
+    round_digit: int = int(input("Rounding digit: ")) 
 
+    calc_errors: str_float_dict = errors(true_value, approx_value, round_digit)
 
-[print(f"{key} = {value}") for key, value in calc_errors.items()]
+    [print(f"{key} = {value}") for key, value in calc_errors.items()]
+    
