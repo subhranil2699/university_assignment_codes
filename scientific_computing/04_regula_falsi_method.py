cabb6c2d414a856acd1842eacd12883906837f52
Subhranil Sarkar <84725647+subhranil2699@users.noreply.github.com>
2022-05-31 00:28:07 +0530

    Update 04_regula_falsi_method.py

--- scientific_computing/04_regula_falsi_method.py
+++ scientific_computing/04_regula_falsi_method.py
@@ -2,10 +2,10 @@
 Method of False Position
 also known as Regula-falsi
 """
-from typing import Callable
+from typing import Callable, Any
 
 
-def regula_falsi(func: Callable[[float], float], point_1: float, point_2: float, err: float) -> float:
+def regula_falsi(func: Callable[[float], float], point_1: float, point_2: float, err: float) -> Any:
     """
     Regula falsi method
     """
@@ -17,7 +17,7 @@ def regula_falsi(func: Callable[[float], float], point_1: float, point_2: float,
         print("n\t|a\t\t\t|b\t\t\t|f(a)\t\t|f(b)\t\t|root\t\t|f(root)\t|")
         while abs(f_r := (func(root := (point_1 * (f_point_2 := func(point_2)) - point_2 * (f_point_1 := func(point_1))) / (f_point_2 - f_point_1)))) >= err:
             if f_r * f_point_1 < 0:
-                print(f"{n}\t|{point_1:0.5f}\t|{point_2:0.5f}\t|{f_point_1:0.5f}\t|{root:0.5f}\t|{f_r:0.5f}\t|")
+                print(f"{n}\t|{point_1:0.5f}\t|{point_2:0.5f}\t|{f_point_1:0.5f}\t|{f_point_2:0.5f}\t|{root:0.5f}\t|{f_r:0.5f}\t|")
                 point_2 = root
             elif f_r * f_point_2 < 0:
                 print(f"{n}\t|{point_1:0.5f}\t|{point_2:0.5f}\t|{f_point_1:0.5f}\t|{f_point_2:0.5f}\t|{root:0.5f}\t|{f_r:0.5f}\t|")
