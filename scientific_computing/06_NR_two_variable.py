863438210e0a0fef3d45aba3c82ec4f3728bb980
Subhranil Sarkar <84725647+subhranil2699@users.noreply.github.com>
2022-06-20 19:22:56 +0530

    Update 06_NR_two_variable.py

--- scientific_computing/06_NR_two_variable.py
+++ scientific_computing/06_NR_two_variable.py
@@ -1,15 +1,15 @@
 import numpy as np
-from numpy import ndarray
-from typing import Any
-
 from sympy import symbols, lambdify, sympify, diff
+import matplotlib.pyplot as plt
+
+plt.style.use('seaborn-whitegrid')
 
 
-def part_diff(expression: str) -> tuple[Any, ...]:
+def part_diff(expression):
     x, y = symbols('x y')
-    f: Any = lambdify([x, y], (f_ := sympify(expression)))
-    f_x: Any = lambdify([x, y], (f_x_ := diff(expression, x)))
-    f_y: Any = lambdify([x, y], (f_y_ := diff(expression, y)))
+    f = lambdify([x, y], (f_ := sympify(expression)))
+    f_x = lambdify([x, y], (f_x_ := diff(expression, x)))
+    f_y = lambdify([x, y], (f_y_ := diff(expression, y)))
 
     print(f"The function is: {f_}")
     print(f"Partial diff w.r.t. x is: {f_x_}")
@@ -18,17 +18,28 @@ def part_diff(expression: str) -> tuple[Any, ...]:
     return f, f_x, f_y
 
 
-def newton_raphson_two(func_1: str, func_2: str, x_k: float, y_k: float, max_iter: int) -> ndarray:
+def newton_raphson_two(func_1, func_2, x_k, y_k, max_iter):
     f, f_x, f_y = part_diff(func_1)
     g, g_x, g_y = part_diff(func_2)
 
-    result: ndarray = np.zeros((2, 1))
+    x_roots: list = []          # empty list for storing the root's x values
+    y_roots: list = []          # empty list for storing the root's y values
+    # initialize the result matrix
+    result = np.zeros((2, 1))
     for i in range(max_iter):
-        X_k: ndarray = np.array([x_k, y_k])[:, np.newaxis]
-        F: ndarray = np.array([f(x_k, y_k), g(x_k, y_k)])[:, np.newaxis]
+        X_k = np.array([x_k, y_k])[:, np.newaxis]  # initial value matrix: column matrix
+        F = np.array([f(x_k, y_k), g(x_k, y_k)])[:, np.newaxis]  # function matrix
 
+        # calculate the value using numpy function 
+        # J_k = np.array([
+        #     [f_x(x_k, y_k), f_y(x_k, y_k)],
+        #     [g_x(x_k, y_k), g_y(x_k, y_k)],
+        # ])
+        # J_k_inv = np.linalg.inv(J_k)
+
+        # Calculating the inverse using formula
         # calculating D_k
-        D_k: float = f_x(x_k, y_k) * g_y(x_k, y_k) - g_x(x_k, y_k) * f_y(x_k, y_k)
+        D_k = f_x(x_k, y_k) * g_y(x_k, y_k) - g_x(x_k, y_k) * f_y(x_k, y_k)
 
         # calculating the jacobian inverse
         J_k_inv = (1 / D_k) * np.array([
@@ -37,25 +48,39 @@ def newton_raphson_two(func_1: str, func_2: str, x_k: float, y_k: float, max_ite
         ])
 
         # setting the next root
-        X_k_1 = X_k - np.dot(J_k_inv, F)
+        X_k_next = X_k - np.dot(J_k_inv, F)
 
-        # updating the values of x_k and y_k
-        x_k, y_k = X_k_1[0, 0], X_k_1[1, 0]
+        # appending the value of roots in the corresponding lists to use them outside the function
+        # to say, plot them
+        
+        x_roots.append(x_k)
+        y_roots.append(y_k)
+        
+        # updating the values of x_k and y_k for the next iteration
+        x_k, y_k = X_k_next[0, 0], X_k_next[1, 0]
 
-        result = X_k_1
+        result = X_k_next
 
-    return result
+    return result, x_roots, y_roots
 
 
 if __name__ == '__main__':
-    func_1: str = "x**2 + x*y + y*y - 7"
-    func_2: str = "x**3 + y**3 - 9"
-
+    func_1 = "x**2 + x*y + y*y - 7"
+    func_2 = "x**3 + y**3 - 9"
     initial_1, initial_2 = 1.5, 0.5
-
     max_iteration: int = 3
 
-    r: ndarray = newton_raphson_two(func_1, func_2, initial_1, initial_2, max_iteration)
+    # result: returns root and list of root's x and y values
+    r = newton_raphson_two(func_1, func_2, initial_1, initial_2, max_iteration)
 
-    print(f"Approximate value of x: {round(r[0, 0], 4)}")
-    print(f"Approximate value of y: {round(r[1, 0], 4)}")
+    root = r[0]
+
+    print(f"Approximate value of x: {round(root[0, 0], 4)}")            # first row, first column: x value
+    print(f"Approximate value of y: {round(root[1, 0], 4)}")            # second row, first column: y value
+
+
+    plt.figure(figsize=(12, 8))
+    plt.plot(r[1], r[2])
+    plt.scatter(r[1], r[2], c='red')
+    plt.title('Newton-Raphson Method for multivariable')
+    plt.show()
